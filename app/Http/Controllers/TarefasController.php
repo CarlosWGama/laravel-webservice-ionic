362f2660c2b8a64ea5f46e9f08977e1ef3c5b64d
Carlos W. Gama <carloswgama@gmail.com>
2018-09-26 18:40:55 -0300

    Iniciado banco - criado migração e models

--- app/Http/Controllers/TarefasController.php
+++ app/Http/Controllers/TarefasController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Usuario;
 
 class TarefasController extends Controller {
 
@@ -11,7 +12,7 @@ class TarefasController extends Controller {
     }
 
     public function cadastrar() {
-
+   
     }
 
     public function editar($id) {
